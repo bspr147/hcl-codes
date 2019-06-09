2158025b9af396923a648617f96649edc0a5e017
Prasanna <prassucp@gmail.com>
2019-06-09 14:45:12 +0530

    testing

---
+++ src_bankProject_testing/BankProject/src/com/hcl/bank/DepositAccountMain.java
@@ -0,0 +1,17 @@
+package com.hcl.bank;
+
+import java.util.Scanner;
+
+public class DepositAccountMain {
+
+	public static void main(String[] args) {
+		int accountNo,depositAmount;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Account No");
+		accountNo=sc.nextInt();
+		System.out.println("Enter Depsoit Amount ");
+		depositAmount=sc.nextInt();
+		System.out.println(AccountBAL.
+				depositAccountBal(accountNo, depositAmount));
+	}
+}
