968d8143616ad0779c3b7a4cdbd9aaef10d736c3
Prasanna <prassucp@gmail.com>
2019-08-07 11:56:00 +0530

    ch

---
+++ Users.sql
@@ -0,0 +1,11 @@
+Create Table Users
+(
+   username varchar(30) primary key,
+   password varchar(30)
+);
+
+Insert into users values('Laksh','Laksh'),
+   ('Kiruba','Laxmi'),
+   ('Anubhav','Anand'),
+   ('Venkat','Sai'),
+   ('Darshini','Priya');
